e3d74a519662cefe53f8a49a093dfa79731bd2e2
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-09-30 17:36:08 +0200

    Gilded Rose tests: GeneralItemTest

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -5,13 +5,38 @@ from gilded_rose import Item, GildedRose
 
 
 class GeneralItemTest(unittest.TestCase):
+    DEGRADE_BY = 1
+
     def setUp(self) -> None:
-        self.item = Item("foo", sell_in=0, quality=0)
+        self.item = Item("foo", sell_in=10, quality=20)
         self.gilded_rose = GildedRose([self.item])
 
-    def test_foo(self):
+    def test_degrades_quality_when_day_passes(self):
+        yesterday_quality = self.item.quality
         self.gilded_rose.update_quality()
-        self.assertEqual("foo", self.item.name)
+        self.assertEqual(self.item.quality, yesterday_quality - self.DEGRADE_BY)
+
+    def test_degrades_twice_as_fast_when_sell_by_passes(self):
+        self.item.sell_in = 0
+        quality_after_sell_deadline = self.item.quality
+
+        self.gilded_rose.update_quality()
+        self.assertEqual(
+            self.item.quality,
+            quality_after_sell_deadline - (self.DEGRADE_BY * 2)
+        )
+
+    def test_quality_never_negative(self):
+        self.item.quality = 1
+
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 0)
+
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 0)
+
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 0)
 
 
 class AgedBrieTest(unittest.TestCase):
