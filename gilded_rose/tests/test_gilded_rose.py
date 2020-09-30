57c0459b6489fcccc00af0516f052ab8f2074d33
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-09-30 16:17:52 +0200

    Gilded Rose: Add lacking test for full coverage

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -6,7 +6,13 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("foo", sell_in=0, quality=0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
+
+    def test_aged_brie_quality_hit_max(self):
+        item = Item("Aged Brie", sell_in=-1, quality=50)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEqual(item.quality, 50)
