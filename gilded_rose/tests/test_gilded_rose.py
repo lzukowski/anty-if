772702727ca5babb4e778ecd108262c4a6dfe947
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-09-30 17:30:29 +0200

    Gilded Rose: split test cases per item type

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -4,15 +4,21 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
+class GeneralItemTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.item = Item("foo", sell_in=0, quality=0)
+        self.gilded_rose = GildedRose([self.item])
+
     def test_foo(self):
-        items = [Item("foo", sell_in=0, quality=0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+        self.gilded_rose.update_quality()
+        self.assertEqual("foo", self.item.name)
+
+
+class AgedBrieTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.item = Item("Aged Brie", sell_in=-1, quality=50)
+        self.gilded_rose = GildedRose([self.item])
 
     def test_aged_brie_quality_hit_max(self):
-        item = Item("Aged Brie", sell_in=-1, quality=50)
-        gilded_rose = GildedRose([item])
-        gilded_rose.update_quality()
-        self.assertEqual(item.quality, 50)
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 50)
