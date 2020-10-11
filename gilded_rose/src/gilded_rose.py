fd92880290df0e1b69ab81a6b849f4a505aad2f7
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-10-11 16:12:26 +0200

    Gilded Rose: Extract goods classes

--- gilded_rose/src/gilded_rose.py
+++ gilded_rose/src/gilded_rose.py
@@ -12,45 +12,97 @@ class Item:
 
 
 class GildedRose:
+    class Generic:
+        quality: int
+        sell_in: int
+
+        def __init__(self, quality: int, sell_in: int) -> None:
+            self.quality = quality
+            self.sell_in = sell_in
+
+        def update(self) -> None:
+            if self.quality > 0:
+                self.quality = self.quality - 1
+            self.sell_in = self.sell_in - 1
+            if self.sell_in < 0:
+                if self.quality > 0:
+                    self.quality = self.quality - 1
+
+    class AgedBrie:
+        quality: int
+        sell_in: int
+
+        def __init__(self, quality: int, sell_in: int) -> None:
+            self.quality = quality
+            self.sell_in = sell_in
+
+        def update(self) -> None:
+            if self.quality < 50:
+                self.quality = self.quality + 1
+            self.sell_in = self.sell_in - 1
+            if self.sell_in < 0:
+                if self.quality < 50:
+                    self.quality = self.quality + 1
+
+    class BackstagePass:
+        quality: int
+        sell_in: int
+
+        def __init__(self, quality: int, sell_in: int) -> None:
+            self.quality = quality
+            self.sell_in = sell_in
+
+        def update(self) -> None:
+            if self.quality < 50:
+                self.quality = self.quality + 1
+                if self.sell_in < 11:
+                    if self.quality < 50:
+                        self.quality = self.quality + 1
+                if self.sell_in < 6:
+                    if self.quality < 50:
+                        self.quality = self.quality + 1
+            self.sell_in = self.sell_in - 1
+            if self.sell_in < 0:
+                self.quality = self.quality - self.quality
+
+    class Sulfuras:
+        quality: int
+        sell_in: int
+
+        def __init__(self, quality: int, sell_in: int) -> None:
+            self.quality = quality
+            self.sell_in = sell_in
+
+        def update(self) -> None:
+            pass
+
     def __init__(self, items: List[Item]) -> None:
         self.items = items
 
     def update_quality(self) -> None:
         for item in self.items:
-            if self._generic(item):
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
+            if self._sulfuras(item):
+                sulfuras = GildedRose.Sulfuras(item.quality, item.sell_in)
+                sulfuras.update()
+                item.quality = sulfuras.quality
+                item.sell_in = sulfuras.sell_in
             elif self._aged_brie(item):
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                aged_brie = GildedRose.AgedBrie(item.quality, item.sell_in)
+                aged_brie.update()
+                item.quality = aged_brie.quality
+                item.sell_in = aged_brie.sell_in
             elif self._backstage_pass(item):
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    item.quality = item.quality - item.quality
-
-    @staticmethod
-    def _generic(item: Item) -> bool:
-        return not (
-            GildedRose._aged_brie(item)
-            or GildedRose._backstage_pass(item)
-            or GildedRose._sulfuras(item)
-        )
+                backstage_pass = GildedRose.BackstagePass(
+                    item.quality, item.sell_in,
+                )
+                backstage_pass.update()
+                item.quality = backstage_pass.quality
+                item.sell_in = backstage_pass.sell_in
+            else:
+                generic = GildedRose.Generic(item.quality, item.sell_in)
+                generic.update()
+                item.quality = generic.quality
+                item.sell_in = generic.sell_in
 
     @staticmethod
     def _aged_brie(item: Item) -> bool:
