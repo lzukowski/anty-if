0d74f6a34073a4a84cde808a1f791a33ffcda7a8
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-10-11 15:53:33 +0200

    Gilded Rose: inline helper methods (leave only type check methods)

--- gilded_rose/src/gilded_rose.py
+++ gilded_rose/src/gilded_rose.py
@@ -18,45 +18,31 @@ class GildedRose:
     def update_quality(self) -> None:
         for item in self.items:
             if self._generic(item):
-                self._handle_generic(item)
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
             elif self._aged_brie(item):
-                self._handle_aged_brie(item)
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
             elif self._backstage_pass(item):
-                self._handle_backstage_pass(item)
-            else:
-                self._handle_sulfuras(item)
-
-    def _handle_generic(self, item: Item) -> None:
-        if self._quality_more_then_0(item):
-            self._decrease_quality(item)
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if self._quality_more_then_0(item):
-                self._decrease_quality(item)
-
-    def _handle_aged_brie(self, item: Item) -> None:
-        if self._quality_less_than_50(item):
-            self._increase_quality(item)
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if self._quality_less_than_50(item):
-                self._increase_quality(item)
-
-    def _handle_backstage_pass(self, item: Item) -> None:
-        if self._quality_less_than_50(item):
-            self._increase_quality(item)
-            if item.sell_in < 11:
-                if self._quality_less_than_50(item):
-                    self._increase_quality(item)
-            if item.sell_in < 6:
-                if self._quality_less_than_50(item):
-                    self._increase_quality(item)
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            self._quality_to_zero(item)
-
-    def _handle_sulfuras(self, item: Item) -> None:
-        pass
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    item.quality = item.quality - item.quality
 
     @staticmethod
     def _generic(item: Item) -> bool:
@@ -77,23 +63,3 @@ class GildedRose:
     @staticmethod
     def _sulfuras(item: Item) -> bool:
         return item.name == "Sulfuras, Hand of Ragnaros"
-
-    @staticmethod
-    def _quality_to_zero(item):
-        item.quality = item.quality - item.quality
-
-    @staticmethod
-    def _quality_more_then_0(item: Item) -> bool:
-        return item.quality > 0
-
-    @staticmethod
-    def _quality_less_than_50(item: Item) -> bool:
-        return item.quality < 50
-
-    @staticmethod
-    def _increase_quality(item: Item) -> None:
-        item.quality = item.quality + 1
-
-    @staticmethod
-    def _decrease_quality(item: Item) -> None:
-        item.quality = item.quality - 1
