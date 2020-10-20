53f85b26b8633417feb7cf17e1a113de9d7be367
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-10-20 11:41:25 +0200

    Refactoring Gilded Rose: remove Sulfuras object

--- gilded_rose/src/gilded_rose/gilded_rose.py
+++ gilded_rose/src/gilded_rose/gilded_rose.py
@@ -10,7 +10,13 @@ class GildedRose:
 
     def update_quality(self) -> None:
         for item in self.items:
+            if self._sulfuras(item):
+                continue
             good = GoodCategory().build_for(item)
             good.update()
             item.quality = good.quality
             item.sell_in = good.sell_in
+
+    @staticmethod
+    def _sulfuras(item: Item) -> bool:
+        return item.name == "Sulfuras, Hand of Ragnaros"
