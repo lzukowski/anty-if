2949d00d883bb33097adf20acc1a542a52ad9c91
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-11-02 15:21:58 +0100

    Refactoring: extract Expired class in every Good class

--- gilded_rose/src/gilded_rose/inventory.py
+++ gilded_rose/src/gilded_rose/inventory.py
@@ -22,37 +22,69 @@ class Quality:
 
 
 class Generic:
-    def update(self, quality: Quality, sell_in: int) -> None:
-        quality.decrease()
-        if sell_in < 0:
+    class Expired:
+        @staticmethod
+        def update(quality: Quality, _: int) -> None:
             quality.decrease()
+            quality.decrease()
+
+    @staticmethod
+    def update(quality: Quality, _: int) -> None:
+        quality.decrease()
+
+    @classmethod
+    def build(cls, sell_in: int) -> Union['Generic', Expired]:
+        if sell_in < 0:
+            return Generic.Expired()
+        return Generic()
 
 
 class AgedBrie:
-    def update(self, quality: Quality, sell_in: int) -> None:
-        quality.increase()
-        if sell_in < 0:
+    class Expired:
+        @staticmethod
+        def update(quality: Quality, _: int) -> None:
             quality.increase()
+            quality.increase()
+
+    @staticmethod
+    def update(quality: Quality, _: int) -> None:
+        quality.increase()
+
+    @classmethod
+    def build(cls, sell_in: int) -> Union['AgedBrie', Expired]:
+        if sell_in < 0:
+            return AgedBrie.Expired()
+        return AgedBrie()
 
 
 class BackstagePass:
-    def update(self, quality: Quality, sell_in: int) -> None:
+    class Expired:
+        @staticmethod
+        def update(quality: Quality, _: int) -> None:
+            quality.reset()
+
+    @staticmethod
+    def update(quality: Quality, sell_in: int) -> None:
         quality.increase()
         if sell_in < 10:
             quality.increase()
         if sell_in < 5:
             quality.increase()
+
+    @classmethod
+    def build(cls, sell_in: int) -> Union['BackstagePass', Expired]:
         if sell_in < 0:
-            quality.reset()
+            return BackstagePass.Expired()
+        return BackstagePass()
 
 
 class GoodCategory:
     def build_for(self, item: Item) -> Union[Generic, AgedBrie, BackstagePass]:
         if self._aged_brie(item):
-            return AgedBrie()
+            return AgedBrie.build(item.sell_in)
         if self._backstage_pass(item):
-            return BackstagePass()
-        return Generic()
+            return BackstagePass.build(item.sell_in)
+        return Generic.build(item.sell_in)
 
     @staticmethod
     def _aged_brie(item: Item) -> bool:
